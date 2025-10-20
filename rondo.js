ec87dd306355e3d16b93a68fbc056214da5e34a7
YASHWANTH PALADUGU <yashwanth@YASHWANTHs-MacBook-Pro.local>
2025-10-20 15:39:29 -0700

    latest UptoDate

--- rondo.js
+++ rondo.js
@@ -1,6 +1,6 @@
 /* Load site/pages/items from Google Sheets (gviz) and render site.
    Items list is independent of pages: no auto-filtering by page. */
-var spreadsheetID = "1KhXNef0QJj48RL1NwmSPaNL9nx0aj_pB10EtzeOZy5w";
+var spreadsheetID = "1iwIx8sqiBTqbtAI7TX4vDGDVee3dPz0wa3MazBv2fzQ";
 var siteSheet  = "1569296108";
 var pagesSheet = "28080804";
 var itemsSheet = "0";
